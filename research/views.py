c486994f8373b4406af2a7c15a52ef07b04702a3
Ramazan Arslan <ramazan@birlikte.al>
2019-01-02 00:47:33 +0300

    word2vec added, html changed

--- research/views.py
+++ research/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
 
-from research.query import simpleQuery, advancedQuery
+from research.query import simpleQuery, advancedQuery, simpleW2V, advancedW2V
 
 
 def index(request):
+    return render(request, 'research/index.html')
+
+
+
+def word2vec_view(request):
     context = None
+
+    if request.method == 'POST':
+
+        # SIMPLE
+        if request.POST['txt_search_input']:
+            response = simpleW2V(query=request.POST)
+
+        # ADVANCED
+        elif request.POST['txt_advanced_search_input']:
+            response = advancedW2V(query=request.POST)
+
+        else:
+            response = None
+
+
+        context = {'query': response}
+
+    return render(request, 'research/word2vec.html', context)
+
+
+
+
+
+
+def query_view(request):
+    context = None
+
+
     if request.method == 'POST':
 
         # SIMPLE
@@ -18,6 +51,9 @@ def index(request):
         else:
             response = None
 
+
         context = {'query': response}
 
-    return render(request, 'research/index.html', context)
+    return render(request, 'research/query.html', context)
+
+
